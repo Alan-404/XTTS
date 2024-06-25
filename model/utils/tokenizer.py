6149f0734f99aae12a42195ee0226997dc7b0260
Đức Trí <nguyentri.alan@gmail.com>
2024-06-25 10:27:05 +0700

    .

--- model/utils/tokenizer.py
+++ model/utils/tokenizer.py
@@ -15,9 +15,6 @@ from spacy.lang.ja import Japanese
 from spacy.lang.zh import Chinese
 from tokenizers import Tokenizer
 
-
-
-
 def get_spacy_lang(lang):
     if lang == "zh":
         return Chinese()
